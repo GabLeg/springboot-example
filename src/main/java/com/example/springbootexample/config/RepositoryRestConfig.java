b68c9e9f5b9175ba0c860d4098c375f533b119b3
GabLeg <gab.legare@gmail.com>
2021-02-22 21:24:27 -0500

    update spring version

--- src/main/java/com/example/springbootexample/config/RepositoryRestConfig.java
+++ src/main/java/com/example/springbootexample/config/RepositoryRestConfig.java
@@ -7,22 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.ProfileResourceProcessor;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class RepositoryRestConfig extends RepositoryRestMvcConfiguration {
-
-    public RepositoryRestConfig(ApplicationContext context, ObjectFactory<ConversionService> conversionService) {
-        super(context, conversionService);
-    }
+public class RepositoryRestConfig implements RepositoryRestConfigurer {
 
     /**
      * Config that make the JSON return the ID with @RepositoryRestResource
+     *
      * @param config config
      */
     @Override
-    public ProfileResourceProcessor profileResourceProcessor(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.exposeIdsFor(Goody.class);
-        return super.profileResourceProcessor(config);
     }
 }
